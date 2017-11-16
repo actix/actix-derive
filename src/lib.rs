caedfbfef27c933918a6c42feab96d1bcc416421
Callym <hi@callym.com>
2017-11-16 16:39:38 +0000

    Add #[derive(Message)], #[Message(response, error)] macros

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use proc_macro::TokenStream;
+
+mod message;
+
+macro_rules! create_derive(
+    ($mod_: ident, $trait_: ident, $fn_name: ident) => {
+        #[proc_macro_derive($trait_)]
+        #[doc(hidden)]
+        pub fn $fn_name(input: TokenStream) -> TokenStream {
+            let s = input.to_string();
+            let ast = syn::parse_derive_input(&s).unwrap();
+            $mod_::expand(&ast).parse().expect("Expanded output was no correct Rust code")
+        }
+    };
+    ($mod_: ident, $trait_: ident, $fn_name: ident, $($attr: ident),+) => {
+        #[proc_macro_derive($trait_, attributes($($attr),+))]
+        #[doc(hidden)]
+        pub fn $fn_name(input: TokenStream) -> TokenStream {
+            let s = input.to_string();
+            let ast = syn::parse_derive_input(&s).unwrap();
+            $mod_::expand(&ast).parse().expect("Expanded output was no correct Rust code")
+        }
+    };
+);
+
+create_derive!(message, Message, message_derive, MessageResult, MessageError, Message);
+
+fn get_attribute_type_multiple(ast: &syn::DeriveInput, name: &str) -> Option<Vec<Option<syn::Ty>>> {
+    let attr = ast.attrs.iter().find(|a| a.name() == name);
+
+    if attr.is_none() {
+        return None;
+    }
+
+    let attr = attr.unwrap();
+
+    if let syn::MetaItem::List(_, ref vec) = attr.value {
+        Some(vec.iter().map(|m| meta_item_to_ty(m, name)).collect())
+    } else {
+        panic!("The correct syntax is #[{}(type, type, ...)]", name);
+    }
+}
+
+fn meta_item_to_ty(meta_item: &syn::NestedMetaItem, name: &str) -> Option<syn::Ty> {
+    if let syn::NestedMetaItem::MetaItem(syn::MetaItem::Word(ref i)) = *meta_item {
+        let ty = syn::parse::ty(i.as_ref());
+        match ty {
+            syn::parse::IResult::Done(_, ty) => Some(ty),
+            _ => None,
+        }
+    } else {
+        panic!("The correct syntax is #[{}(type)]", name);
+    }
+}
---
+++ src/message.rs
@@ -0,0 +1,28 @@
+use quote;
+use syn;
+
+pub const MESSAGE_ATTR: &str = "Message";
+
+pub fn expand(ast: &syn::DeriveInput) -> quote::Tokens {
+    let (item_type, error_type) = {
+        let ty = ::get_attribute_type_multiple(ast, MESSAGE_ATTR).unwrap();
+        match ty.len() {
+            1 => (ty[0].clone(), None),
+            2 => (ty[0].clone(), ty[1].clone()),
+            _ => panic!("#[{}(type, type)] takes 2 parameters, given {}", MESSAGE_ATTR, ty.len()),
+        }
+    };
+
+    let name = &ast.ident;
+    let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+
+    let item_type = item_type.unwrap_or(syn::Ty::Tup(vec![]));
+    let error_type = error_type.unwrap_or(syn::Ty::Tup(vec![]));
+
+    quote!{
+        impl #impl_generics ResponseType for #name #ty_generics #where_clause {
+            type Item = #item_type;
+            type Error = #error_type;
+        }
+    }
+}
