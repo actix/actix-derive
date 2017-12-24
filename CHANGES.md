5ec5e3d3c9925c513141a4a2c8a9109fcc6fd0a7
Nikolay Kim <fafhrd91@gmail.com>
2017-12-23 18:25:44 -0800

    move tests to actix

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Changes
 
+## 0.1.1 (2017-12-23)
+
+* Move tests to actix
+
 ## 0.1.0 (2017-12-23)
 
 * Added `msg` proc macro
--- build.rs
+++ build.rs
@@ -1,29 +1,6 @@
-extern crate skeptic;
 extern crate version_check;
 
-use std::{env, fs};
 
-
-#[cfg(unix)]
-fn main() {
-    let f = env::var("OUT_DIR").unwrap() + "/skeptic-tests.rs";
-    if env::var("USE_SKEPTIC").is_ok() {
-        let _ = fs::remove_file(f);
-        // generates doc tests for `README.md`.
-        skeptic::generate_doc_tests(
-            &["README.md"]);
-    } else {
-        let _ = fs::File::create(f);
-    }
-
-    match version_check::is_nightly() {
-        Some(true) => println!("cargo:rustc-cfg=actix_nightly"),
-        Some(false) => (),
-        None => (),
-    };
-}
-
-#[cfg(not(unix))]
 fn main() {
     match version_check::is_nightly() {
         Some(true) => println!("cargo:rustc-cfg=actix_nightly"),
