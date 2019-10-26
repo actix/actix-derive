95adc78b3a32c5196765742c98a011d5e7ddf1fc
DanSnow <dododavid006@gmail.com>
2019-10-26 18:18:40 +0800

    chore: Update syn, quote to 1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 #![recursion_limit = "128"]
 
 extern crate proc_macro;
-extern crate proc_macro2;
-#[macro_use]
-extern crate syn;
-#[macro_use]
-extern crate quote;
 
 use proc_macro::TokenStream;
 use syn::DeriveInput;
--- src/message_response.rs
+++ src/message_response.rs
@@ -1,13 +1,9 @@
-use proc_macro2::{Ident, Span, TokenStream};
-use syn;
+use proc_macro2::TokenStream;
+use quote::quote;
+use syn::parse_quote;
 
 pub fn expand(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
-
-    let dummy_const = Ident::new(
-        &format!("_IMPL_ACT_MSG_RSP_{}", name).to_lowercase(),
-        Span::call_site(),
-    );
 
     let (_, ty_generics, where_clause) = ast.generics.split_for_impl();
 
@@ -19,15 +15,10 @@ pub fn expand(ast: &syn::DeriveInput) -> TokenStream {
     let (impl_generics, _, _) = generics.split_for_impl();
 
     quote! {
-        mod #dummy_const {
-            use super::*;
-            extern crate actix;
-
-            impl #impl_generics actix::dev::MessageResponse<_A, _M> for #name #ty_generics #where_clause {
-                fn handle<R: actix::dev::ResponseChannel<_M>>(self, _: &mut _A::Context, tx: Option<R>) {
-                    if let Some(tx) = tx {
-                        tx.send(self);
-                    }
+        impl #impl_generics ::actix::dev::MessageResponse<_A, _M> for #name #ty_generics #where_clause {
+            fn handle<R: actix::dev::ResponseChannel<_M>>(self, _: &mut _A::Context, tx: Option<R>) {
+                if let Some(tx) = tx {
+                    tx.send(self);
                 }
             }
         }
